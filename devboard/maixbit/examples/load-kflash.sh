d1f00f3d4266acb9055b535200286309a723843b
Michał Derkacz <michal@Lnet.pl>
2020-05-20 00:49:09 +0200

    max programming speed seems to be 750 kB/s

--- devboard/maixbit/examples/load-kflash.sh
+++ devboard/maixbit/examples/load-kflash.sh
@@ -4,5 +4,5 @@ set -e
 
 name=$(basename $(pwd))
 objcopy -O binary $name.elf $name.bin
-kflash -p /dev/ttyUSB0 -b 1500000 -B bit_mic $name.bin
+kflash -p /dev/ttyUSB0 -B bit_mic -b 750000 $name.bin
 rm $name.bin
