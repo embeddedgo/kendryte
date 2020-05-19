ad59b645493043d222b04b2e0b414a3c540bbd05
Michał Derkacz <michal@Lnet.pl>
2020-05-20 00:50:05 +0200

    blink all LEDs

--- devboard/maixbit/examples/blinky/main.go
+++ devboard/maixbit/examples/blinky/main.go
@@ -4,35 +4,78 @@ import (
 	"github.com/embeddedgo/kendryte/p/fpioa"
 	"github.com/embeddedgo/kendryte/p/gpio"
 	"github.com/embeddedgo/kendryte/p/sysctl"
+	"github.com/embeddedgo/kendryte/p/uart"
 )
 
-var d uint64
+var d1, d2 uint64
 
-func delay() {
-	stop := d + 1e7
-	for d != stop {
-		d++
+func delay1() {
+	end := d1 + 1e7
+	for d1 != end {
+		d1++
+	}
+}
+
+func delay2() {
+	end := d2 + 3e7
+	for d2 != end {
+		d2++
 	}
 }
 
 func main() {
-	SYSCTL := sysctl.SYSCTL()
-	SYSCTL.APB0_CLK_EN().Set()
-	SYSCTL.CLK_EN_PERI.Store(sysctl.FPIOA_CLK_EN | sysctl.GPIO_CLK_EN)
+	sctl := sysctl.SYSCTL()
+	sctl.APB0_CLK_EN().Set()
+	sctl.CLK_EN_PERI.Store(sysctl.FPIOA_CLK_EN | sysctl.GPIO_CLK_EN)
 
-	const FUNC_GPIO3 = 59
+	const (
+		FUNC_GPIO1 = 57
+		FUNC_GPIO2 = 58
+		FUNC_GPIO3 = 59
+	)
 
-	redPin := &fpioa.FPIOA().IO[13]
-	redPin.Store(FUNC_GPIO3<<fpioa.CH_SELn | 15<<fpioa.DSn | fpioa.OE_EN |
+	green := &fpioa.FPIOA().IO[12]
+	green.Store(FUNC_GPIO1<<fpioa.CH_SELn | 15<<fpioa.DSn | fpioa.OE_EN |
+		fpioa.PD)
+	red := &fpioa.FPIOA().IO[13]
+	red.Store(FUNC_GPIO2<<fpioa.CH_SELn | 15<<fpioa.DSn | fpioa.OE_EN |
+		fpioa.PD)
+	blue := &fpioa.FPIOA().IO[14]
+	blue.Store(FUNC_GPIO3<<fpioa.CH_SELn | 15<<fpioa.DSn | fpioa.OE_EN |
 		fpioa.PD)
 
 	GPIO := gpio.GPIO()
-	GPIO.DIRECTION.SetBits(1 << 3)
-	GPIO.DATA_OUTPUT.SetBits(1 << 3)
+	GPIO.DATA_OUTPUT.SetBits(1<<1 + 1<<2 + 1<<3)
+	GPIO.DIRECTION.SetBits(1<<1 + 1<<2 + 1<<3)
 	for {
-		delay()
-		GPIO.DATA_OUTPUT.ClearBits(1 << 3)
-		delay()
 		GPIO.DATA_OUTPUT.SetBits(1 << 3)
+		GPIO.DATA_OUTPUT.ClearBits(1 << 1)
+		delay2()
+		GPIO.DATA_OUTPUT.SetBits(1 << 1)
+		GPIO.DATA_OUTPUT.ClearBits(1 << 2)
+		delay2()
+		GPIO.DATA_OUTPUT.SetBits(1 << 2)
+		GPIO.DATA_OUTPUT.ClearBits(1 << 3)
+		delay2()
+		uartPutc('A')
+		uartPutc('\r')
+		uartPutc('\n')
 	}
+}
+
+func uartPutc(c byte) {
+	u := uart.UART3()
+	for u.LSR.LoadBits(1<<5) != 0 {
+	}
+	u.RBR_DLL_THR.Store(uart.RBR_DLL_THR(c))
+}
+
+func uartInit() {
+	SYSCTL := sysctl.SYSCTL()
+	SYSCTL.APB0_CLK_EN().Set()
+	SYSCTL.UART3_CLK_EN().Set()
+
+	u := uart.UART3()
+	u.LCR.SetBits(1 << 7)
+	//...
 }
