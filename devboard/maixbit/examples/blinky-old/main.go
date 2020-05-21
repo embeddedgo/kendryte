ff20f7ba3162eacf79d2fe5309697442e0c7b95a
Michał Derkacz <michal@Lnet.pl>
2020-05-21 18:53:31 +0200

    new blinky examle

--- devboard/maixbit/examples/blinky-old/main.go
+++ devboard/maixbit/examples/blinky-old/main.go
@@ -1,29 +1,49 @@
 package main
 
 import (
-	"embedded/arch/riscv/systim"
-	"time"
+	"embedded/mmio"
+	"unsafe"
 
 	"github.com/embeddedgo/kendryte/p/fpioa"
 	"github.com/embeddedgo/kendryte/p/gpio"
 	"github.com/embeddedgo/kendryte/p/sysctl"
+	"github.com/embeddedgo/kendryte/p/uart"
 )
 
-func main() {
-	sctl := sysctl.SYSCTL()
+func MulDiv(x, m, d uint64) uint64 {
+	divx := x / d
+	modx := x - divx*d
+	divm := m / d
+	modm := m - divm*d
+	return divx*m + modx*divm + modx*modm/d
+}
 
-	cpuHz := int64(26e6)
-	if sctl.ACLK_SEL().Load() != 0 {
-		pll := sctl.PLL[0].Load()
+func nanotime() int64 {
+	sc := sysctl.SYSCTL()
+	cpuHz := uint64(26e6)
+	if sc.ACLK_SEL().Load() != 0 {
+		pll := sc.PLL[0].Load()
 		r := uint64(pll&sysctl.CLKR)>>sysctl.CLKRn + 1
 		f := uint64(pll&sysctl.CLKF)>>sysctl.CLKFn + 1
 		od := uint64(pll&sysctl.CLKOD)>>sysctl.CLKODn + 1
 		pllHz := 26e6 * f / (r * od)
-		aclkDivSel := sctl.ACLK_DIVIDER_SEL().Load() >> sysctl.ACLK_DIVIDER_SELn
-		cpuHz = int64(pllHz / 2 << aclkDivSel)
+		aclkDivSel := sc.ACLK_DIVIDER_SEL().Load() >> sysctl.ACLK_DIVIDER_SELn
+		cpuHz = pllHz / 2 << aclkDivSel
 	}
-	systim.Setup(cpuHz / 50)
+	clintHz := cpuHz / 50
+	mtime := (*mmio.U64)(unsafe.Pointer(uintptr(0x2000000 + 0xBFF8))).Load()
+	return int64(MulDiv(mtime, 1e9, clintHz))
+}
 
+func waitUntil(t int64) {
+	for nanotime() < t {
+	}
+}
+
+var lastt uint64
+
+func main() {
+	sctl := sysctl.SYSCTL()
 	sctl.APB0_CLK_EN().Set()
 	sctl.CLK_EN_PERI.Store(sysctl.FPIOA_CLK_EN | sysctl.GPIO_CLK_EN)
 
@@ -47,18 +67,36 @@ func main() {
 	GPIO.DATA_OUTPUT.SetBits(1<<1 + 1<<2 + 1<<3)
 	GPIO.DIRECTION.SetBits(1<<1 + 1<<2 + 1<<3)
 
+	t := nanotime()
 	for {
 		GPIO.DATA_OUTPUT.SetBits(1 << 3)
 		GPIO.DATA_OUTPUT.ClearBits(1 << 1)
-		time.Sleep(time.Second)
-
+		t += 1e9
+		waitUntil(t)
 		GPIO.DATA_OUTPUT.SetBits(1 << 1)
 		GPIO.DATA_OUTPUT.ClearBits(1 << 2)
-		time.Sleep(time.Second)
-
+		t += 1e9
+		waitUntil(t)
 		GPIO.DATA_OUTPUT.SetBits(1 << 2)
 		GPIO.DATA_OUTPUT.ClearBits(1 << 3)
-		time.Sleep(time.Second)
-
+		t += 1e9
+		waitUntil(t)
 	}
+}
+
+func uartPutc(c byte) {
+	u := uart.UART3()
+	for u.LSR.LoadBits(1<<5) != 0 {
+	}
+	u.RBR_DLL_THR.Store(uart.RBR_DLL_THR(c))
+}
+
+func uartInit() {
+	SYSCTL := sysctl.SYSCTL()
+	SYSCTL.APB0_CLK_EN().Set()
+	SYSCTL.UART3_CLK_EN().Set()
+
+	u := uart.UART3()
+	u.LCR.SetBits(1 << 7)
+	//...
 }
