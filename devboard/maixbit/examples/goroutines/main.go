bc110dd024aedc69d4798dbf7a81ae0626b3d034
Michał Derkacz <michal@Lnet.pl>
2020-05-25 19:01:12 +0200

    maixbit/examples/goroutines: new examle

---
+++ devboard/maixbit/examples/goroutines/main.go
@@ -0,0 +1,28 @@
+// Copyright 2020 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"runtime"
+	"time"
+
+	"github.com/embeddedgo/kendryte/devboard/maixbit/board/leds"
+)
+
+func blink(led leds.LED, delay time.Duration) {
+	runtime.LockOSThread()
+	for {
+		led.SetOn()
+		time.Sleep(delay)
+		led.SetOff()
+		time.Sleep(delay)
+	}
+}
+
+func main() {
+	go blink(leds.Red, time.Second*2)
+	go blink(leds.Green, time.Second/3)
+	blink(leds.Blue, time.Second/11)
+}
