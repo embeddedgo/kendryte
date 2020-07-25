322213761a352e6526796829fd83b8188ef39ae0
Jeffrey Jenner <thetooth@ameoto.com>
2020-07-25 11:11:19 +0930

    Rewrite driver for new periph

--- hal/timer/driver.go
+++ hal/timer/driver.go
@@ -4,51 +4,22 @@
 
 package timer
 
-import (
-	"github.com/embeddedgo/kendryte/p/bus"
-	"github.com/embeddedgo/kendryte/p/timer"
-)
-
-type Driver struct {
-	p *Periph
-
-	channel int
+type DriverPWM struct {
+	*Channel
 }
 
-func NewDriver(p *Periph, ch int) *Driver {
-	d := Driver{
-		p:       p,
-		channel: ch,
-	}
-
-	return &d
+func PWM(ch *Channel) *DriverPWM {
+	return &DriverPWM{Channel: ch}
 }
 
-func (d *Driver) EnablePWM() {
-	d.p.EnableClock()
+func (d *DriverPWM) Enable() {
+	d.Periph().EnableClock()
 
-	bits := timer.PWM_ENABLE | timer.ENABLE | timer.USER | timer.INTERRUPT
-	d.p.CH[d.channel].CONTROL.SetBits(bits)
+	d.control.SetBits(PWM_ENABLE | ENABLE | USER | INTERRUPT)
 }
 
-func (d *Driver) EnableISR() {
-	d.p.EnableClock()
-
-	// Avoid interrupt storm if frequency has not been set
-	if d.p.CH[d.channel].LOAD.Load() == 0 || d.p.LOAD_COUNT2[d.channel].Load() == 0 {
-		d.SetFrequency(1, .5)
-	}
-
-	// Clear any existing ISRs
-	d.p.ResetISR(d.channel)
-
-	// Enable timer in user mode, unset interrupt mask if it was set
-	d.p.CH[d.channel].CONTROL.SetBits(timer.ENABLE | timer.USER)
-	d.p.CH[d.channel].CONTROL.ClearBits(timer.INTERRUPT)
-}
-
-func (d *Driver) SetFrequency(frequency int, duty float64) {
-	clk := bus.APB0.Clock()
+func (d *DriverPWM) SetFrequency(frequency float64, duty float64) {
+	clk := float64(d.Periph().Bus().Clock() * 2)
 
 	if frequency < 0 || frequency > 2147483647 {
 		panic("pwm: frequency outside of 32bit range")
@@ -56,9 +27,9 @@ func (d *Driver) SetFrequency(frequency int, duty float64) {
 	if duty < 0 || duty > 1 {
 		panic("pwm: duty cycle must be 0.0-1.0")
 	}
-	period := uint32(clk) / uint32(frequency)
+	period := uint32(clk / frequency)
 	percent := uint32(duty * float64(period))
 
-	d.p.CH[d.channel].LOAD.Store(timer.LOAD(period - percent))
-	d.p.LOAD_COUNT2[d.channel].Store(timer.LOAD_COUNT2(percent))
+	d.load_count.Store(period - percent)
+	d.Periph().load_count2[d.n()].Store(percent)
 }
