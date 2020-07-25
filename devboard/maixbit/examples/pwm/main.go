b20a02429c35380c39fe9faf2a3a891a65c164df
Jeffrey Jenner <thetooth@ameoto.com>
2020-07-25 11:09:28 +0930

    Update example

--- devboard/maixbit/examples/pwm/main.go
+++ devboard/maixbit/examples/pwm/main.go
@@ -19,14 +19,14 @@ var tickCount uint64
 
 //go:interrupthandler
 func TIMER0A_Handler() {
-	timer.TIMER(0).ResetISR(0)
-	timer.TIMER(0).ResetISR(1)
+	timer.TIMER(0).ClearIRQ(0)
+	timer.TIMER(0).ClearIRQ(1)
 
 	tickCount++
 }
 
 func main() {
-	freq := 100 // Hz
+	freq := 100.0 // Hz
 
 	// Pin assignment
 	ch1 := fpioa.Pin(12)
@@ -36,36 +36,35 @@ func main() {
 	ch2.Setup(fpioa.TIMER0_TOGGLE1 | fpioa.EnOE | fpioa.DriveH34L23)
 	ch3.Setup(fpioa.TIMER0_TOGGLE3 | fpioa.EnOE | fpioa.DriveH34L23)
 
+	// Peripheral
+	p := timer.TIMER(0)
+
 	// Driver instance
-	r := timer.NewDriver(timer.TIMER(0), 0)
-	g := timer.NewDriver(timer.TIMER(0), 1)
-	b := timer.NewDriver(timer.TIMER(0), 2)
+	r := timer.PWM(p.Channel(0))
+	g := timer.PWM(p.Channel(1))
+	b := timer.PWM(p.Channel(2))
 
 	// Set frequency now so we don't have to wait around for the first period to expire
 	r.SetFrequency(freq, .5)
 	g.SetFrequency(freq, .5)
 	b.SetFrequency(freq, .5)
 
-	// Animate duty cycle
-	go func() {
-		dc := 0.0
-		for {
-			r.SetFrequency(freq, math.Abs(math.Sin(dc)))
-			g.SetFrequency(freq, math.Abs(math.Sin(dc+math.Pi)))
-			b.SetFrequency(freq, math.Abs(math.Cos(dc)))
-			dc += 0.02
-			time.Sleep(10 * time.Millisecond)
-		}
-	}()
-
 	// Enable the timer and PWM function
-	r.EnablePWM()
-	g.EnablePWM()
-	b.EnablePWM()
+	r.Enable()
+	g.Enable()
+	b.Enable()
 
 	// Enable an ISR for the green channel
-	g.EnableISR()
+	g.EnableIRQ()
 	irq.TIMER0A.Enable(rtos.IntPrioLow, irq.M0)
 
-	select {}
+	// Animate duty cycle
+	dc := 0.0
+	tick := time.NewTicker(10 * time.Millisecond)
+	for range tick.C {
+		r.SetFrequency(freq, math.Abs(math.Sin(dc)))
+		g.SetFrequency(freq, math.Abs(math.Sin(dc+math.Pi)))
+		b.SetFrequency(freq, math.Abs(math.Cos(dc)))
+		dc += 0.02
+	}
 }
