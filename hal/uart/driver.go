8f8e457a191039097d024b6e7c427792d6d84cb8
Michał Derkacz <michal@Lnet.pl>
2020-07-31 09:55:11 +0200

    hal/uart: start working on Driver

---
+++ hal/uart/driver.go
@@ -0,0 +1,50 @@
+// Copyright 2020 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package uart
+
+type DriverError uint8
+
+const (
+	// ErrBufOverflow is returned if one or more received bytes has been dropped
+	// because of the lack of free space in the driver's receive buffer.
+	ErrBufOverflow DriverError = iota + 1
+
+	// ErrTimeout is returned if timeout occured. It means that the read/write
+	// operation has been interrupted. In case of write you can not determine
+	// the exact number of bytes sent to the remote party.
+	ErrTimeout
+)
+
+// Error implements error interface.
+func (e DriverError) Error() string {
+	switch e {
+	case ErrBufOverflow:
+		return "uart: buffer overflow"
+	case ErrTimeout:
+		return "uart: timeout"
+	}
+	return ""
+}
+
+type Driver struct {
+	p *Periph
+
+	// rx state
+	rxbuf    []byte
+	nextr    uint32
+	nextw    uint32
+	rxcmd    uint32
+	overflow uint32
+	rxready  rtos.Note
+
+	// tx state
+	txdata string
+	txn    int
+	txdone rtos.Note
+
+	isr       uint32
+	timeoutRx time.Duration
+	timeoutTx time.Duration
+}
