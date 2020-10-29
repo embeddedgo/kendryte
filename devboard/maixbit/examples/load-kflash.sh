7c48d4a0d2aff81925661e7caeb2beb7bf8b655b
Michał Derkacz <michal@Lnet.pl>
2020-10-29 01:07:08 +0100

    load-kflash.sh: pass optional arguments to kflash

--- devboard/maixbit/examples/load-kflash.sh
+++ devboard/maixbit/examples/load-kflash.sh
@@ -4,5 +4,5 @@ set -e
 
 name=$(basename $(pwd))
 objcopy -O binary $name.elf $name.bin
-kflash -p /dev/ttyUSB0 -B bit_mic -b 750000 $name.bin
+kflash -p /dev/ttyUSB0 -B bit_mic -b 750000 $@ $name.bin
 rm $name.bin
